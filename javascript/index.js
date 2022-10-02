d60eba887501b3785f56f1fc61f5e22337ec5246
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-02 21:05:20 +0530

    jk

---
+++ javascript/index.js
@@ -0,0 +1,21 @@
+var a;
+a=10;
+console.log(a);
+console.log(typeof a);
+
+a="hello";
+console.log(a);
+console.log(typeof a);
+
+a=true;
+console.log(a);
+console.log(typeof a);
+
+a=null;
+console.log(a);
+console.log(typeof a);
+
+// var num=10;
+// console.log(num);
+// num float=2.4;
+// console.log(float);
