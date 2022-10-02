99a35389525895229b02a20565b1b438caddf217
seehimanshu <himanshu.thakur2812@gmail.com>
2022-10-02 21:28:06 +0530

    javascript

--- javascript/index.js
+++ javascript/index.js
@@ -15,7 +15,25 @@ a=null;
 console.log(a);
 console.log(typeof a);
 
-// var num=10;
-// console.log(num);
-// num float=2.4;
-// console.log(float);
+var num=10;
+console.log(num);
+var float=2.4;
+console.log(float);
+
+var str='a';
+console.log(str);
+console.log(typeof str);
+str="how you doing"
+console.log(str);
+
+var b="hello g kidda";
+console.log(b);
+var b="hello g\nkidda";
+console.log(b);
+
+b=`kya kr re
+ho g`
+console.log(b);
+
+var num=540;
+console.log(`half of ${num} is ${num/2}`);
